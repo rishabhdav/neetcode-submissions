5383a381206653caad114a1aa2606d50593ace34
Rishabh tripathi <rishabhtripathi763@gmail.com>
2026-07-10 17:48:12 +0530

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int start=0;
+        int end=s.length()-1;
+        int cnt=0;
+        while(start<=end){
+            if(isSame(s.charAt(start),s.charAt(end))){
+                start++;
+                end--;
+            }
+            else{
+                if(cnt<1){
+               if(isSame(s.charAt(start),s.charAt(end-1))){
+                cnt++;
+                 end--;
+                   }
+                   else  if(isSame(s.charAt(start+1),s.charAt(end))){
+                    cnt++;
+                   start++;
+                   }
+                   else return false;
+                }
+                else return false;
+            }
+        }
+        return true;
+
+
+    }
+    public boolean isSame(char a,char b){
+        if(a>='A'&&a<='Z') a=(char)(a+32);
+        if(b>='A'&&b<='Z') b=(char)(b+32);
+        return a==b;
+    }
+}
