d31562e37fa501ac8ce285b2799af76eaaae3708
Rishabh tripathi <rishabhtripathi763@gmail.com>
2026-07-13 14:41:01 +0530

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+
+public String encode(List<String> strs) {
+
+
+StringBuilder str=new StringBuilder();
+for(int i=0;i<strs.size();i++){
+   
+  if(!strs.get(i).isEmpty()) str.append(strs.get(i)).append(",");
+}
+
+return str.toString();
+    }
+
+public List<String> decode(String str) {
+  
+ArrayList<String>list=new ArrayList<>();
+String[] arr=str.split(",");
+for(int i=0;i<arr.length;i++){
+    list.add(arr[i]);
+}
+return list;
+    }
+}
