7cf83742986cdadcf9564559262a5d5ec66a4759
Rishabh tripathi <rishabhtripathi763@gmail.com>
2026-07-17 12:10:48 +0530

    Add: design-circular-queue - submission-1

---
+++ Data Structures & Algorithms/design-circular-queue/submission-1.java
@@ -0,0 +1,85 @@
+class ListNode{
+    int val;
+    ListNode next;
+
+    ListNode(int val){
+        this.val=val;
+        this.next=null;
+    }
+}
+
+class MyCircularQueue {
+    int space;
+    ListNode left;
+    ListNode right;
+
+
+    public MyCircularQueue(int k) {
+    this.space=k;
+    this.left=new ListNode(0);
+    this.right=this.left;
+        
+    }
+    
+    public boolean enQueue(int value) {
+
+        if(isFull()) return false;
+        ListNode newNode=new ListNode(value);
+        
+        right.next=newNode;
+        right=newNode;
+        space--;
+        return true;
+    }
+    
+    public boolean deQueue() {
+        if(isEmpty()) return false;
+         if(this.left.next==null) {
+            this.left.next=null;
+            right=left;
+            space++;
+            return true;
+         }
+
+         else{
+
+            this.left.next=this.left.next.next;
+         ;
+            space++;
+            return true;
+         }
+
+        
+    }
+    
+    public int Front() {
+        if(isEmpty()) return -1;
+        return this.left.next.val;
+        
+    }
+    
+    public int Rear() {
+          if(isEmpty()) return -1;
+        return this.right.val;
+    }
+    
+    public boolean isEmpty() {
+        return this.left==this.right?true:false;
+        
+    }
+    
+    public boolean isFull() {
+        return this.space==0?true:false;
+    }
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
+ */
