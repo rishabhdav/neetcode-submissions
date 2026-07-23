3f929cfbe8946147669293f5976e4ad775e3b064
Rishabh tripathi <rishabhtripathi763@gmail.com>
2026-07-23 11:28:19 +0530

    Add: bitwise-and-of-numbers-range - submission-0

---
+++ Data Structures & Algorithms/bitwise-and-of-numbers-range/submission-0.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int rangeBitwiseAnd(int left, int right) {
+        int result=left;
+        for(int i=left+1;i<=right;i++){
+            result=result&i;
+        }
+        return result;
+    }
+}
