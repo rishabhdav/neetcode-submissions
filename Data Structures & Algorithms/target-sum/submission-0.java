346a657990511df5bf5eb99771627597c27a2bc7
Rishabh tripathi <rishabhtripathi763@gmail.com>
2026-08-01 16:49:56 +0530

    Add: target-sum - submission-0

---
+++ Data Structures & Algorithms/target-sum/submission-0.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int totalsum=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            totalsum=totalsum+nums[i];
+        }
+         target=totalsum-target;
+        target=target/2;
+        int[][] dp=new int[n+1][target+1];
+        for(int i=0;i<n;i++){
+            dp[i][0]=1;
+        }
+        if(nums[0]<=target){
+            dp[0][nums[0]]=1;
+        }
+
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=target;j++){
+                int pick=Integer.MIN_VALUE;
+                 if(j>=nums[i]){
+                    pick=dp[i-1][j-nums[i]];
+                 }
+                 int notpick=dp[i-1][j];
+                 dp[i][j]=Math.max(pick,notpick);
+            }
+        }
+        return dp[n-1][target];
+
+    }
+}
