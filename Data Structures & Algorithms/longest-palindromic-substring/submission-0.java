99c21eabcd131cbf683da3de95b553bb7d4cd3c1
Rishabh tripathi <rishabhtripathi763@gmail.com>
2026-08-04 13:51:36 +0530

    Update: longest-palindromic-substring - submission-0

--- Data Structures & Algorithms/longest-palindromic-substring/submission-0.java
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-0.java
@@ -1,36 +1,34 @@
 class Solution {
     public String longestPalindrome(String s) {
-       StringBuilder str=new StringBuilder(s);
-       str=str.reverse();
-       String newstr=str.toString();
-       int n=s.length();
-       int[][] dp=new int[n+1][n+1];
-
-       for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            if(s.charAt(i-1)==newstr.charAt(j-1)){
-                dp[i][j]=1+dp[i-1][j-1];
+        int len = 0;
+        int startlen = 0;
+        int l = 0;
+        int r = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            l = i;
+            r = i;
+            while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
+                if (len < r - l + 1) {
+                    startlen = l;
+                    len = r - l + 1;
+                }
+                l--;
+                r++;
             }
-            else{
-                dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+            l = i;
+            r = i
+                + 1;
+
+                while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
+                if (len < r - l + 1) {
+                    startlen = l;
+                    len = r - l + 1;
+                }
+                l--;
+                r++;
             }
         }
-       }
-     StringBuilder str1=new StringBuilder();
-     int i=n;
-     int j=n;
-     while(i>1||j>1){
-        if(s.charAt(i-1)==newstr.charAt(j-1)){
-            str1.insert(0,s.charAt(i-1));
-            i--;
-            j--;
-
-        }
-        else if(dp[i-1][j]>dp[i][j-1]){
-            i--;
-        }
-        else j--;
-     }
-return str1.toString();
+        return s.substring(startlen,startlen+len);
     }
 }
