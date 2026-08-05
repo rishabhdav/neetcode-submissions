ec795d79cef21c96a7440fbb2082771af530ff8b
Rishabh tripathi <rishabhtripathi763@gmail.com>
2026-08-05 20:09:35 +0530

    Add: perfect-squares - submission-1

---
+++ Data Structures & Algorithms/perfect-squares/submission-1.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public int numSquares(int n) {
+        while (n % 4 == 0) {
+            n /= 4;
+        }
+
+        if (n % 8 == 7) {
+            return 4;
+        }
+
+        if (isSquareNum(n)) {
+            return 1;
+        }
+
+        for (int i = 1; i * i <= n; i++) {
+            if (isSquareNum(n - i * i)) {
+                return 2;
+            }
+        }
+
+        return 3;
+    }
+
+    private boolean isSquareNum(int num) {
+        int s = (int) Math.sqrt(num);
+        return s * s == num;
+    }
+}
